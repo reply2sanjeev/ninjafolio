b9b070347c78b81633e6e58ac4d77c17ad2c029e
reply2sanjeev <reply2sanjeev@live.com>
2023-02-20 10:20:32 -0500

    First checkout

---
+++ src/components/Loader/LoaderLogo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./LoaderLogo.css";
+
+class LogoLoader extends React.Component {
+  render() {
+    const theme = this.props.theme;
+    return (
+      <>
+       Hi
+      </>
+    );
+  }
+}
+
+export default LogoLoader;
